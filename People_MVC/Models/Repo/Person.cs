f1b1850f925f8d85508fb786d4835e3df435dff9
Wei Cao <viviancao0728@gmail.com>
2021-09-01 21:02:12 +0200

    Add project files.

---
+++ People_MVC/Models/Repo/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People_MVC.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get;set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+
+
+        [Required]
+        [MaxLength(20)]
+        public string TeleNumber { get; set; }
+    }
+}
